6ba8fbbc76b4493723e80a2b9d04deadba599850
404J <unknownc4m@gmail.com>
2026-09-15 16:56:04 +0900

    MedicalHistory 엔티티 작성

---
+++ src/main/java/com/hospital/hospitalreservation/domain/MedicalHistory.java
@@ -0,0 +1,39 @@
+package com.hospital.hospitalreservation.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "medical_histories")
+public class MedicalHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "history_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String diagnosis;
+
+    @Column(nullable = false)
+    private String prescription;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id", unique = true)
+    private Reservation reservation;
+
+    protected MedicalHistory() {
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+}
