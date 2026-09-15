7869e0a505b22772b90a4df47d75d7be0a969028
404J <unknownc4m@gmail.com>
2026-09-15 16:04:38 +0900

    Reservation 엔티티 작성

---
+++ src/main/java/com/hospital/hospitalreservation/domain/Reservation.java
@@ -0,0 +1,59 @@
+package com.hospital.hospitalreservation.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "reservations")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @Column(name = "reservation_date", nullable = false)
+    private LocalDate reservationDate;
+
+    @Column(name = "reservation_time", nullable = false)
+    private LocalTime reservationTime;
+
+    @Column(nullable = false, length = 20)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    protected Reservation(){
+    }
+
+    public Reservation(LocalDate reservationDate, LocalTime reservationTime, String status, Member member){
+        this.reservationDate = reservationDate;
+        this.reservationTime = reservationTime;
+        this.status = status;
+        this.member = member;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getReservationDate() {
+        return reservationDate;
+    }
+
+    public LocalTime getReservationTime() {
+        return reservationTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+}
