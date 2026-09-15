8bd85d2f295d194b62bc72e686a32352aca28edf
404J <unknownc4m@gmail.com>
2026-09-15 16:29:21 +0900

    Hospital 엔티티 작성

---
+++ src/main/java/com/hospital/hospitalreservation/domain/Hospital.java
@@ -0,0 +1,48 @@
+package com.hospital.hospitalreservation.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "hospitals")
+public class Hospital {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hospital_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false, length = 20)
+    private String tel;
+
+    protected Hospital() {
+    }
+
+    public Hospital(String name, String address, String tel) {
+        this.name = name;
+        this.address = address;
+        this.tel = tel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+}
