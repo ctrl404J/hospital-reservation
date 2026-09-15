1f51c9249b0b570371b711d3e06e44d320af9972
404J <unknownc4m@gmail.com>
2026-09-15 16:19:45 +0900

    Doctor 엔티티 작성

---
+++ src/main/java/com/hospital/hospitalreservation/domain/Doctor.java
@@ -0,0 +1,40 @@
+package com.hospital.hospitalreservation.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "doctors")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "doctor_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String specialty;
+
+    protected Doctor(){
+    }
+
+    public Doctor(String name, String specialty) {
+        this.name = name;
+        this.specialty = specialty;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+}
