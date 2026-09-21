fef3d8b1b0ebefb3cdbfa656124ef8476f0ed0eb
404J <unknownc4m@gmail.com>
2026-09-22 04:15:59 +0900

    refactor: 삭제로직을 베이스엔티티가 아닌 애노테이션으로 하도록 수정

--- src/main/java/com/hospital/hospitalreservation/domain/BaseEntity.java
+++ src/main/java/com/hospital/hospitalreservation/domain/BaseEntity.java
@@ -9,10 +9,6 @@ public abstract class BaseEntity {
     @Column(name = "is_Deleted", nullable = false)
     private boolean isDeleted = false;
 
-    public void delete(){
-        this.isDeleted = true;
-    }
-
     public void restore(){
         this.isDeleted = false;
     }
